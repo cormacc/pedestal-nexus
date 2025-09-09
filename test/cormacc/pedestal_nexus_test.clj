458d74afc0d368263cb8d19b6bb6fb64de4a873a
Cormac Cannon <cormacc@gmail.com>
2025-09-09 22:53:57 +0100

    refactor: Send response via channel rather than promise
    We were using a promise to deliver the http response from nexus to
    pedestal. This worked fine, but would have caused the thread to block.
    
    Pedestal allows an interceptor to return a channel and exit without
    waiting for the response to arrive. This changeset replaces the promise
    with a channel, which should in theory allow for better use of
    resources.
    
    This changeset also verifies that the list of actions includes a
    :http/response effect before returning the channel, to prevent waiting
    on a value from a channel that will never arrive.
    When the effect isn't found, the context is returned as is instead,
    allowing pedestal's default response handling to kick in.

--- test/cormacc/pedestal_nexus_test.clj
+++ test/cormacc/pedestal_nexus_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer [deftest is testing]]
             [cormacc.pedestal-nexus :as sut])) ; system under test
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest includes-action
+  (testing "Should return false given no actions"
+    (is (false? (sut/includes-action [] :http/response))))
+  (testing "Should return false given no matching actions"
+    (is (false? (sut/includes-action [[:bla/bla 1]
+                                      [:bleh/bleh 2 4 3]]
+                                     :http/response))))
+  (testing "Should return true given single matching action"
+    (is (true? (sut/includes-action [[:http/response :ok "Bla"]]
+                                    :http/response))))
+  (testing "Should return true given nested match"
+    (is (true? (sut/includes-action [[:do/something 9]
+                                     [:do/something-async {:then [:http/response :ok "Bla"]}]]
+                                    :http/response))))
+;;
+  )
